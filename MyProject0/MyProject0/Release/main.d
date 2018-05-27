a24b095e0a13b213e28045de81b4d2bb97eac404
florent.tainturier <florent.tainturier@gmail.com>
2018-05-27 16:18:24 +0200

    main.c PIN principle

--- MyProject0/MyProject0/Release/main.d
+++ MyProject0/MyProject0/Release/main.d
@@ -1,5 +1,4 @@
-main.d main.o: .././main.c ../atmel_start.h ../include/driver_init.h \
- ../utils/compiler.h \
+main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
@@ -11,25 +10,9 @@ main.d main.o: .././main.c ../atmel_start.h ../include/driver_init.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\builtins.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- ../utils/interrupt_avr8.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
- ../Config/clock_config.h ../include/port.h ../include/atmel_start_pins.h \
- ../include/sysctrl.h ../utils/atomic.h ../include/protected_io.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sleep.h \
- ../include/atmel_start_pins.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
-
-../atmel_start.h:
-
-../include/driver_init.h:
-
-../utils/compiler.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -52,34 +35,6 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\builtins.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
-
-../utils/interrupt_avr8.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
-
-../Config/clock_config.h:
-
-../include/port.h:
-
-../include/atmel_start_pins.h:
-
-../include/sysctrl.h:
-
-../utils/atomic.h:
-
-../include/protected_io.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sleep.h:
-
-../include/atmel_start_pins.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
--- MyProject0/MyProject0/main.c
+++ MyProject0/MyProject0/main.c
@@ -11,12 +11,20 @@ int main(void)
 	
 	DDRA |=(1<<DDA0);	//A0 as output
 	DDRD |=(1<<DDD0);	//A0 as output
+	DDRD |=(1<<DDD1);	//A0 as output
 
 	/* Replace with your application code */
 	while (1) {
-		PINA |=(1<<PINA0);	//inverse bit
-		PIND |=(1<<PIND0);
-		_delay_ms(5000);
+		
+		PORTA |=(1<<PORTA0);
+		//PINA |=(1<<PINA0);	//inverse bit
+		//PIND |=(1<<PIND0);
+		_delay_ms(1000);
+		_delay_ms(1000);
+		PORTD |=(1<<PORTD0);
+		PORTD |=(1<<PORTD1);
+		_delay_ms(500);
+		PORTD = 0x00;        // Turn OFF diodes on PORTD
 		
 			
 	}
